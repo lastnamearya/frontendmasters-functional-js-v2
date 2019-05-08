9ee1bd208fef8021907130abaca9e00927002669
lastnamearya <arya.jigyasu6815@gmail.com>
2019-05-08 20:23:46 +0530

    Passing function as key on Objects

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
-var box = {};
+obj = {};
 
-// We can use Bracket Notation on an Object to define Properties on an Object
-box["material"] = "cardboard";
+// Js will accept this function as key and stringify it.
+obj[function() {}] = false;
 
-// We can use Dot Notation on an Object to define Properties on an Object.
-box.color = "red";
-
-// Here Numeric Value is going to be coerced to a String Value as Property.
-box[0] = "Zero";
-
-box["$^&"] = "testing `123`";
-
-// We can't assign a string as a property on Object using Dot Notation. It'll throw Unexpected Error;
-
-// box."abc" = "Not Possible";
-
-// Dot Notation behind the scenes covert that Numeric or passed value into String behind the scenes.
-
-// For that we've to use Brakcet Notation
-box["abc"] = "Not Possible";
-
-// We can write an Expression in Bracket Notation.
-box[2 + 2] = 4;
-
-console.log(box);
+// It'll coerced to a String Value. We can't call this function later on.
+console.log(typeof Object.keys(obj)[0]);
