ae3eb40e301ea89cd2cd38682f23ab3176ee7372
lastnamearya <arya.jigyasu6815@gmail.com>
2019-05-08 19:28:55 +0530

    Array ~ Bracket Notation

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-// We can assign Properties ( Key: Value ) Pairs to an Array.
 var person = [];
+var plea = "wouldShe";
 
 person.name = "Mrs. White";
 
 var who = person.name;
 
-console.log(who);
+// That's how we create Properties on our Array Object.
+person["hello"] = "HI";
 
-// typeof an Array is always be Object.
-console.log(typeof person);
+// Here 0 will be stringified to "0"
+person[0] = "I was not in the Billiards room";
+
+person[plea] = "I would never!";
+
+console.log(person);
+
+// person.length will be 1 and "hello" & plea ("wouldShe") are going to be properties.
+
+var x = [];
+var b = 1;
+
+// Here Numeric Value is going as an Entry in Memory Location of Array.
+x[b] = "hello";
+
+console.log(x);
