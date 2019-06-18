62b68cad22b0645e15e6a57ba9782309dec7c0f3
lastnamearya <arya.jigyasu6815@gmail.com>
2019-06-19 02:49:50 +0530

    _.each( ) ~ Documented

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
 // My own made _.each() Utility function
 
-const _ = {};
-
-_.each = function(list, callback) {
-  if (typeof list === "object") {
-    if (Array.isArray(list)) {
-      for (let i = 0; i < list.length; i++) {
-        callback(list[i]);
-      }
-    } else {
-      for (let key in list) {
-        callback(list[key]);
-      }
-    }
-  }
-};
-
 const exArr = [1, 2, 3, 4];
 
 const exObj = {
@@ -25,9 +9,40 @@ const exObj = {
   d: 4
 };
 
-_.each(exArr, function(item) {
-  console.log(item);
+const _ = {};
+
+_.each = function(list, callback) {
+  // First We'll check if it's an Array and Object or not by checking type to continue with
+
+  if (typeof list === "object") {
+    // Then, we check if it's an Array Data Structure or not?
+
+    if (Array.isArray(list)) {
+      // Then we loop over the Arugment Array and pass that indices to our calllback function as arugment.
+
+      for (let i = 0; i < list.length; i++) {
+        // We can additionally also pass index and whole array itself as well.
+
+        callback(list[i], i, list);
+      }
+    } else {
+      // Else, If it's not array then it's going to be Object itself.
+
+      for (let key in list) {
+        // Passing Object value to the callback as argument.
+        callback(list[key], key, list);
+      }
+    }
+  }
+};
+
+// Testing with Array
+
+_.each(exArr, function(item, i, list) {
+  console.log(`${item} is ${i + 1} value of [${list}] array.`);
 });
+
+// Testing with Object
 
 _.each(exObj, function(item) {
   console.log(item);
