dc44e64f97e1d436c9618923238c8ddd685ac57b
lastnamearya <arya.jigyasu6815@gmail.com>
2019-06-19 22:40:03 +0530

    My own ~ _.map() function

--- app.js
+++ app.js
@@ -1,49 +1,40 @@
-// My own made _.each() Utility function
+var exArr = [1, 2, 3, 4];
 
-const exArr = [1, 2, 3, 4];
-
-const exObj = {
+var exObj = {
   a: 1,
   b: 2,
   c: 3,
   d: 4
 };
 
-const _ = {};
+const _ = new Object();
 
 _.each = function(list, callback) {
-  // First We'll check if it's an Array and Object or not by checking type to continue with
-
   if (typeof list === "object") {
-    // Then, we check if it's an Array Data Structure or not?
-
     if (Array.isArray(list)) {
-      // Then we loop over the Arugment Array and pass that indices to our calllback function as arugment.
-
       for (let i = 0; i < list.length; i++) {
-        // We can additionally also pass index and whole array itself as well.
-
         callback(list[i], i, list);
-      }
-    } else {
-      // Else, If it's not array then it's going to be Object itself.
-
-      for (let key in list) {
-        // Passing Object value to the callback as argument.
-        callback(list[key], key, list);
       }
     }
   }
 };
 
-// Testing with Array
+_.map = function(list, callback) {
+  if (typeof list === "object" && Array.isArray(list)) {
+    const returnArr = [];
+    for (let i = 0; i < list.length; i++) {
+      returnArr.push(callback(i));
+    }
+    return returnArr;
+  }
+};
 
-_.each(exArr, function(item, i, list) {
-  console.log(`${item} is ${i + 1} value of [${list}] array.`);
+_.each(exArr, function(i) {
+  console.log(i);
 });
 
-// Testing with Object
-
-_.each(exObj, function(item) {
-  console.log(item);
+const result = _.map(exArr, function(i) {
+  return i + 1;
 });
+
+console.log(result);
