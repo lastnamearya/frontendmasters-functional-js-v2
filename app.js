d94b66b0ef9f1a09f6c1fbcd25562acd2c6183ab
lastnamearya <arya.jigyasu6815@gmail.com>
2019-06-23 16:32:49 +0530

    Compilation Pass in JavaScript before Interpetation Pass

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-// _.map() Method ~ Array
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(arrFunc);
 
-const exArr = [1, 5, 7, 6, 4, 9, 2, 13, 17, 12, 19, 18];
+// The reason in Hoisting we get undefined on Line 1 is because assignmet will happen during Interpetation Phase.
+var x = 1;
 
-const _ = {};
+// Functions are Special Case where whole body of the function can be accessed above on line 2. Actually Now I got the reason ~ Here assignmet happens in Interpetatio Phase.
 
-_.map = function(list, callback) {
-  if (Array.isArray(list)) {
-    let mappedArr = [];
-    for (let i = 0; i < list.length; i++) {
-      mappedArr.push(callback(list[i]));
-    }
-    return mappedArr;
-  }
+function y() {
+  console.log("y");
+}
+
+// Objects ~ Still we get undefined on Line 3.
+var z = {
+  a: 1
 };
 
-const resultArr = _.map(exArr, function(i) {
-  return i + 1;
-});
-
-console.log(resultArr);
+// Here Assignment will happen in the Interpetation Step, so you'll get undefined on line 4.
+var arrFunc = () => console.log("Arr Function");
