2f1b705f03e3e56888f94fc00f19bd1e403a8c55
lastnamearya <arya.jigyasu6815@gmail.com>
2019-06-28 17:09:12 +0530

    Length of Js Parameters

--- app.js
+++ app.js
@@ -1,66 +1,19 @@
-// _.map() Method ~ Array
+// Test ~ Function Parameter's length
 
-const exArr = [5, 1, 3, 7, 4];
+function test(a, b) {
+  console.log(`${a} ${b}`);
+}
 
-const _ = {};
+// In case of spread operator, Js will avoid ...dots
 
-// Default Implementation only accepts an Array
+function test2(a, b, ...args) {}
 
-_.sort = function(input) {
-  if (Array.isArray(input)) {
-    const sortedArray = [];
+// Js will return 0 here
 
-    let resultArr = [];
+function test3(...args) {}
 
-    function test() {
-      let list = Array.from(input);
-      let iterable = 0;
+console.log(test.length);
 
-      for (let i = 0; i < list.length; i += 2) {
-        let res = compare(list[i], list[i + 1]);
+console.log(test2.length);
 
-        if (res < 0) {
-          sortedArray.push(list[i]);
-          sortedArray.push(list[i + 1]);
-        } else if (res > 0) {
-          sortedArray.push(list[i + 1]);
-          sortedArray.push(list[i]);
-        } else {
-          sortedArray.push(list[i]);
-        }
-
-        iterable++;
-
-        if (iterable == Math.ceil(list.length / 2)) {
-          console.log("loop over finally");
-          resultArr = Array.from(sortedArray);
-          sortedArray.length = [];
-          console.log(sortedArray);
-        }
-      }
-    }
-
-    function compare(a, b) {
-      if (a < b) {
-        return -1;
-      } else if (a > b) {
-        return 1;
-      } else if (a == b) {
-        return 0;
-      }
-    }
-
-    for (let i = 0; i < input.length; i++) {
-      console.log(`loop ran ${i} time`);
-      test();
-    }
-
-    test();
-
-    return resultArr;
-  }
-};
-
-const result = _.sort(exArr);
-
-console.log(result);
+console.log(test3.length);
