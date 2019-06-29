13db75320c7a946c2b0c04aa936addd305ff2503
lastnamearya <arya.jigyasu6815@gmail.com>
2019-06-29 16:03:45 +0530

    :fire: ~ My own reduce function for additon and multiplication

--- app.js
+++ app.js
@@ -15,3 +15,49 @@ const isTrue = args => console.log(args);
 const isFalse = args => console.log(args);
 
 ifElse(true, isTrue, isFalse, ["one", "two", "three"]);
+
+// My own Reduce Implementation
+
+const _ = {};
+
+_.reduce = function(list, callback, initialValue) {
+  if (Array.isArray(list)) {
+    let callbackString = callback.toString();
+
+    const stringArr = [...callbackString];
+
+    let result = initialValue;
+
+    if (stringArr.includes("+")) {
+      for (let i = 0; i < list.length; i++) {
+        result += callback(initialValue, list[i]);
+      }
+    } else if (stringArr.includes("*")) {
+      for (let i = 0; i < list.length; i++) {
+        result *= callback(initialValue, list[i]);
+      }
+    }
+
+    return result;
+  }
+};
+
+const reduceSum = _.reduce(
+  [1, 2, 3, 4],
+  function(a, b) {
+    return a + b;
+  },
+  0
+);
+
+const reduceMultiple = _.reduce(
+  [1, 2, 3, 4],
+  function(a, b) {
+    return a * b;
+  },
+  1
+);
+
+console.log(reduceSum);
+
+console.log(reduceMultiple);
