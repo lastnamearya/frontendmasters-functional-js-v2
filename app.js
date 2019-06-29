1ff977452f4666050bfb73dd123163c5b217edb0
lastnamearya <arya.jigyasu6815@gmail.com>
2019-06-29 12:53:16 +0530

    HOF ~ Passing as an argument

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ const isTrue = () => console.log(true);
 const isFalse = () => console.log(false);
 
 ifElse(true, isTrue, isFalse);
+
+// Passing Arguments
+
+var doMathSoIDontHaveTo = function(n, func) {
+  return func(n);
+};
+
+var increment = function(n) {
+  return n + 1;
+};
+
+var square = function(n) {
+  return n * n;
+};
+
+console.log(doMathSoIDontHaveTo(4, increment));
+console.log(doMathSoIDontHaveTo(4, square));
