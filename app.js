0c86b7ac05dd02ad20d6a9a3e19ae0c41ecb1f00
lastnamearya <arya.jigyasu6815@gmail.com>
2019-06-29 13:01:36 +0530

    HOF ~ refactore to ES6 Syntax

--- app.js
+++ app.js
@@ -12,17 +12,11 @@ ifElse(true, isTrue, isFalse);
 
 // Passing Arguments
 
-var doMathSoIDontHaveTo = function(n, func) {
-  return func(n);
-};
+const doMathSoIDontHaveTo = (n, func) => func(n);
 
-var increment = function(n) {
-  return n + 1;
-};
+const increment = n => n + 1;
 
-var square = function(n) {
-  return n * n;
-};
+const square = n => n * n;
 
 console.log(doMathSoIDontHaveTo(4, increment));
 console.log(doMathSoIDontHaveTo(4, square));
